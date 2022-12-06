4227767400e14b261e4b1a9148cd9cd89f8a6386
JoshGonzalez <56143006+JoshGonzalez@users.noreply.github.com>
2022-12-05 22:02:54 -0800

    Party Supplies

---
+++ PartyStarter.py
@@ -0,0 +1,30 @@
+import subprocess
+
+filepath = 'Party.py'
+try:
+    while True: # input("Enter any key to continue...") != KeyboardInterrupt:
+        subprocess.call(filepath)  # Start the party
+        # Wait for file to end
+        input("Enter any key to continue...")  # wait for user input
+except KeyboardInterrupt:
+    print("Terminated")
+
+
+def forward(): # Forward button is pressed and the program increments the message array
+    with open("Fuel.txt") as f:
+        lines = f.readlines()
+        index = int(lines[0])
+        if index > len(lines):
+            lines[0] = index + 1
+    with open("Fuel.txt", "w") as f:
+        f.writelines(lines)
+
+
+def back(): # Back button is pressed and the program decrements the message array
+    with open("Fuel.txt") as f:
+        lines = f.readlines()
+        index = int(lines[0])
+        if index != 0:
+            lines[0] = index - 1
+    with open("Fuel.txt", "w") as f:
+        f.writelines(lines)
