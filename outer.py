baa0e4d36edd14113d0d86ea41c3c9eeeec1a8da
ashleykuewa <60330113+ashleykuewa@users.noreply.github.com>
2022-12-06 00:01:50 -0800

    fixed appearance and speed

--- outer.py
+++ outer.py
@@ -7,9 +7,10 @@ lines = textArray[1:]
 
 
 root = tk.Tk()
-deli = 300           # milliseconds of delay per character
+deli = 200           # milliseconds of delay per character
 svar = tk.StringVar()
-labl = tk.Label(root, textvariable=svar, fg='pink', height=1000, width= 1000, bg='black', anchor='center', font=('Courier 20'))
+labl = tk.Label(root, textvariable=svar, height=1000, width=1000,
+                fg='hot pink', bg='lavender', anchor='center', font=('Courier 100'))
 root.attributes('-fullscreen', True)
 
 
@@ -24,5 +25,5 @@ def shif():
 shif.msg = lines[index]
 shif()
 labl.pack()
-root.after(3000, lambda: root.quit()) #Run for 3 seconds
+root.after(8000, lambda: root.quit()) #Run for 8 seconds
 root.mainloop()
